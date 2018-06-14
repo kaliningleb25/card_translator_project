8d08811f3bb43d8283c4a13ad74e4d8bec4cbd02
glebkalinin <kaliningleb25@gmail.com>
2018-06-14 12:35:23 +0300

    initialize commit

---
+++ src/main/java/com/kaliningleb25/github/card_translator_project/CardTranslatorProjectApplication.java
@@ -0,0 +1,25 @@
+package com.kaliningleb25.github.card_translator_project;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
+
+import java.util.ArrayList;
+import java.util.List;
+
+@SpringBootApplication
+public class CardTranslatorProjectApplication extends SpringBootServletInitializer {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CardTranslatorProjectApplication.class, args);
+	}
+
+
+}
+
+
+
+
+
+
+
